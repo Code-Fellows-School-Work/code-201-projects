10bb5f16636adee746416c9cad5bf1de020fe358
Errol Vidad <errolvidad@gmail.com>
2023-10-25 23:20:54 +0000

    made a lot of changes

--- class3/js/apps.js
+++ class3/js/apps.js
@@ -1,22 +1,26 @@
 //ensures strict JS standard
 'use strict';
 
-//asks user their name upon entering website
+// asks user their name upon entering website
 let userName = prompt('What is your name?');
 console.log(userName);
 alert('Welcome to my website ' + userName);
 
-//y or n question using if.. else statement
+//created variable to start answer correct answer counter
+let correctAnswers = 0;
+
+// y or n question using if.. else statement
 let dog = prompt('Do I own a mini dachshund?');
 dog = dog.toLowerCase();
 console.log(dog);
 if (dog === 'yes' || dog === 'y') {
   alert('Correct, I do!');
+  correctAnswers++;
 } else {
   alert('Nope, I do');
 }
 
-//y or n question using switch statement
+// y or n question using switch statement
 let flavor = prompt('Is tonkotsu my favorite ramen flavor?');
 flavor = flavor.toLowerCase();
 console.log(flavor);
@@ -24,22 +28,24 @@ switch (flavor) {
 case 'yes':
 case 'y':
   alert('Yes and it\'s also my dog\'s name too!');
+  correctAnswers++;
   break;
 default:
   alert('It really is actually');
 }
 
-//y or n question using if.. else statement
+// y or n question using if.. else statement
 let city = prompt('Do I currently live in Kailua, HI?');
 city = city.toLowerCase();
 console.log(city);
 if (city === 'yes' || city === 'y') {
   alert('I wish, but not correct!');
+  correctAnswers++;
 } else {
   alert('Yep, I no longer live there');
 }
 
-//y or n question using switch statement
+// y or n question using switch statement
 let experience = prompt('Did I serve in the Navy?');
 experience = experience.toLowerCase();
 console.log(experience);
@@ -47,12 +53,13 @@ switch (city) {
 case 'yes':
 case 'y':
   alert('Correct for 9 years!');
+  correctAnswers++;
   break;
 default:
   alert('Nope I did for 9 years');
 }
 
-//y or n question using switch statement
+// y or n question using switch statement
 let code = prompt('Is my favorite coding language CSS?');
 code = code.toLowerCase();
 console.log(code);
@@ -60,17 +67,60 @@ switch (code) {
 case 'no':
 case 'n':
   alert('Right, who really likes that anyways?');
+  correctAnswers++;
   break;
 default:
   alert('Haha defintely not');
 }
 
-//number question asking user to guess a number and responding if number is too high or too low
-//also has 4 attemps otherwise give answer to user after 4th incorrect attempt
+// number question asking user to guess a number and responding if number is too high or too low
+// also has 4 attemps otherwise give answer to user after 4th incorrect attempt
 let myAge = 31;
-for (let i = 1; i < 5; i++){
-  console.log(i);
+for (let i = 1; i <= 4; i++){
+  let userGuess = prompt('Guess my age');
+  console.log(userGuess);
+  if (userGuess == myAge) {
+    alert('You got it right!');
+    correctAnswers++;
+    break;
+  } else if (userGuess < myAge) {
+    alert('Too low, try again');
+  } else if (userGuess > myAge) {
+    alert('Too high, try again');
+  }
+  if (i === 4) {
+    alert('Sorry that was your 4th attempt, the correct answer is 31.');
+  }
 }
 
-//personally thanking user with added name for answering all questions
-alert('Thanks for answering all those questions about me, ' + userName);
+//created loop for array question
+let visitedCountries = ['Japan', 'Kwajalein Atoll', 'Guam', 'Philippines', 'South Korea', 'Hong Kong', 'Thailand', 'France', 'Switzerland', 'Greece'];
+let guessedCorrect = false;
+
+for (let i = 0; i < 6; i++) {
+  let countryGuess = prompt('Guess a country that I have visited');
+  console.log(countryGuess);
+  if (visitedCountries.includes(countryGuess)) {
+    alert('You guessed right!');
+    correctAnswers++;
+    guessedCorrect = true;
+    break;
+  } else {
+    alert('Please guess again');
+  }
+}
+
+if (i === 6 && !guessedCorrect) {
+  alert("Sorry, that was your 6th attempt. Here are all the countries I've visited: ");
+  for (let i = 0; i < visitedCountries.length; i++) {
+    alert(visitedCountries[i]);
+  }
+}
+
+// lists the array within the console log
+// for (let i = 0; i < visitedCountries.length; i++){
+//   alert(visitedCountries[i]);
+// }
+
+// personally thanking user with added name for answering all questions
+alert('Thanks for playing! Correct answers: ' + correctAnswers);
